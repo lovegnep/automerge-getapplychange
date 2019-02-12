61ead86e1fd49f1d2dc018ea5b63b559ee2525ed
jipeng <jipeng@teambition.com>
2019-02-12 17:13:44 +0800

    ddd

--- src/client.js
+++ src/client.js
@@ -113,7 +113,13 @@ class Client extends React.Component {
         if (!this.socket) {
             console.log('connecting...')
             this.clientId = `client:${this.props.clientId}-${uuid()}`
-            this.socket = io(url, {query: {clientId: this.clientId},forceNew:true})
+            this.socket = io(url, {
+                query: {
+                    clientId: this.clientId
+                },
+                forceNew:true,
+                transports: ['polling']
+            })
         }
         this.socket.on('connect',()=>{
             console.log('连接成功');
